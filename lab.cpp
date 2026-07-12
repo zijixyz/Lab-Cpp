3140008208b58f1ee66a59bce8335c5c4a7256d6
Ziji <newbaong3@gmail.com>
2026-07-12 13:37:41 +0700

    Add files via upload

---
+++ lab.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+
+using namespace std;
+
+int x = 100;
+int y = 200;
+
+int main() {
+    if (x > y) {
+        cout << "x is greater than y" << endl;
+    } else {
+        cout << "x is not greater than y" << endl;
+    }
+
+    return 0;
+}
